cfe950cd0af7b8b86be4c7e7356e07aa5ba72325
chilliyo <coxonerica@gmail.com>
2017-11-09 23:55:13 -0600

    fixing menu switching

--- src/frontend/components/App/index.jsx
+++ src/frontend/components/App/index.jsx
@@ -21,6 +21,7 @@ function App(props) {
 	var output_fromSciunit=['sciunit create Project1'];
 	const { output, runCommand,cmd_detail,clickedButton} = props;
 	const {menu_selection,cmd_id_M} = props;
+	console.log(typeof(cmd_id_M));
 	// cmd_id = cmd_id_M;
 	const outputChildren = output.map(o => {
 		var user_input_command = parse(o.value);
