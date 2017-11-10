df19c099bb01724877af385537ef3607935f807e
chilliyo <coxonerica@gmail.com>
2017-11-10 13:43:54 -0600

    trip back issue

--- src/frontend/components/App/index.jsx
+++ src/frontend/components/App/index.jsx
@@ -101,24 +101,24 @@ function App(props) {
 		}
 
 		else if (e.key === 'Enter' &&  user_input_command == button_ref.value && command_list_restrict.includes(user_input_command)) {
-			if (cmd_id<length_of_cmd_detail-1){
-				cmd_id+=1;
-				if (cmd_id == length_of_cmd_detail-1){
+			if (cmd_id < length_of_cmd_detail - 1) {
+				cmd_id += 1;
+				if (cmd_id == length_of_cmd_detail - 1) {
 					progress_bar_percentage = "100%";
 				}
-				else{
-					var percen = parseInt(progress_bar_percentage.slice(0,2));
-					percen = percen+10;
+				else {
+					var percen = parseInt(progress_bar_percentage.slice(0, 2));
+					percen = percen + 10;
 					progress_bar_percentage = percen.toString() + "%";
 					// console.log(progress_bar_percentage);
 				}
 				// console.log('cmd_id', cmd_id);
 			}
-			else
-			{
+			else {
 				cmd_id = 0;
 				// console.log(cmd_id);
 			}
+		}
 
 
 	};
