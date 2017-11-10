9c97f2070a240e4253cf1fff733ddaeac5d5a39b
chilliyo <coxonerica@gmail.com>
2017-11-09 23:44:47 -0600

    fixing menu switching

--- src/frontend/actions/menu_selection.js
+++ src/frontend/actions/menu_selection.js
@@ -2,6 +2,6 @@ export const menu_selection = (cmd_detail_id) => {
 	console.log("You clicked on button: ", cmd_detail_id.target.getAttribute('value'));
 	return{
 		type: "MENU_SELECTED",
-		payload: cmd_detail_id.target.getAttribute('value')
+		payload: parseInt(cmd_detail_id.target.getAttribute('value'))
 	}
 };
