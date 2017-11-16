15a467bc084979ae4860fae4a38ee737df807f8e
chilliyo <coxonerica@gmail.com>
2017-11-15 22:29:45 -0600

    try to change cwd by process.chdir

--- src/backend/CommandRunner.js
+++ src/backend/CommandRunner.js
@@ -26,9 +26,18 @@ CommandRunner.prototype.run = function run({ command, _spawn = spawn }) {
 		shell: this.shell
 	});
 
-	console.log(commandName,args);
-	console.log(process.cwd());
+	// console.log(commandName,args);
+	// console.log(process.cwd());
+	// var current_directory = "/home/ubuntu/try.sciunit_10262017/test_cwd";
 
+	console.log('Starting directory: ' + process.cwd());
+	try {
+		process.chdir('/test_cwd');
+		console.log('New directory: ' + process.cwd());
+	}
+	catch (err) {
+		console.log('chdir: ' + err);
+	}
 	// console.log("child_process.env",process.env);
 
 	childProcess.stdout.on('data', data => this.emit('output', data.toString()));
