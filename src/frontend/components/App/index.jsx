2be96f837b0112750e4ada7e62a1189a352f02a4
chilliyo <coxonerica@gmail.com>
2017-11-17 17:30:50 -0600

    printing a msg

--- src/frontend/components/App/index.jsx
+++ src/frontend/components/App/index.jsx
@@ -117,7 +117,7 @@ function App(props) {
 		// console.log(user_input_command);
 		// console.log(cmd_id);
 
-		// console.log(user_input_command.trim(" "),button_ref.value.trim(" "),user_input_command.trim(" "),("sicunit open "+copy_token).trim(" "),user_input_command === button_ref.value, user_input_command === "sicunit open "+copy_token);
+		console.log(user_input_command.trim(" "),button_ref.value.trim(" "),user_input_command.trim(" "),("sicunit open "+copy_token).trim(" "),user_input_command === button_ref.value, user_input_command === "sicunit open "+copy_token);
 		console.log(user_input_command.slice(0,13) === "sicunit open ");
 		if (e.key === 'Enter' && user_input_command.slice(0,13) === "sicunit open "){
 			progress_bar_percentage = "100%";
