65f03feb725aaa8dc20928b00da194831e113a0b
chilliyo <coxonerica@gmail.com>
2017-11-17 17:45:54 -0600

    ok

--- src/frontend/components/App/index.jsx
+++ src/frontend/components/App/index.jsx
@@ -116,15 +116,14 @@ function App(props) {
 	const nextStep = (e) => {
 		// console.log(user_input_command);
 		// console.log(cmd_id);
-		// console.log(user_input_command.trim(" "),button_ref.value.trim(" "),user_input_command.trim(" "),("sicunit open "+copy_token).trim(" "),user_input_command === button_ref.value, user_input_command === "sicunit open "+copy_token);
+		// console.log(,button_ref.value.trim(" "),user_input_command.trim(" "),("sicunit open "+copy_token).trim(" "),user_input_command === button_ref.value, user_input_command === "sicunit open "+copy_token);
 
-		console.log(user_input_command.slice(0,13),user_input_command.slice(0,13).length);
-		console.log("sicunit open ");
-		if (e.key === 'Enter' && cmd_id === length_of_cmd_detail - 3){
+		// console.log(user_input_command.slice(0,13),user_input_command.slice(0,13).length);
+		console.log(user_input_command.trim(" ").slice(0,12)==="sciunit open");
+		if (e.key === 'Enter' ){
 			progress_bar_percentage = "100%";
 			cmd_id += 1;
 		}
-
 		else if (e.key === 'Enter' &&  user_input_command == button_ref.value && command_list_restrict.includes(user_input_command)) {
 			if (cmd_id < length_of_cmd_detail - 1) {
 				cmd_id += 1;
