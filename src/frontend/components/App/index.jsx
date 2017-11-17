2b185e385971e49b761a1390a016c592803261e1
chilliyo <coxonerica@gmail.com>
2017-11-17 17:24:04 -0600

    cmd_id increment after sciunit open

--- src/frontend/components/App/index.jsx
+++ src/frontend/components/App/index.jsx
@@ -118,7 +118,7 @@ function App(props) {
 		// console.log(cmd_id);
 
 		// console.log(user_input_command.trim(" "),button_ref.value.trim(" "),user_input_command.trim(" "),("sicunit open "+copy_token).trim(" "),user_input_command === button_ref.value, user_input_command === "sicunit open "+copy_token);
-		if (e.key === 'Enter' && (user_input_command == button_ref.value)===false && (user_input_command.slice(0,12) === "sicunit open")===false){
+		if (e.key === 'Enter' &&  (user_input_command === "sicunit open "+copy_token)){
 			if (cmd_id == length_of_cmd_detail-1){
 				progress_bar_percentage = "100%";
 				cmd_id += 1;
