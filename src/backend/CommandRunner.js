868061b283b13004f229f305abba52fa0ddc192b
chilliyo <coxonerica@gmail.com>
2017-11-17 18:13:04 -0600

    copy files

--- src/backend/CommandRunner.js
+++ src/backend/CommandRunner.js
@@ -29,20 +29,20 @@ CommandRunner.prototype.run = function run({ command, _spawn = spawn }) {
 
 	if (commandName == "sciunit" && args[2] === "create")
 	{
-		console.log("hello form create",command);
+		// console.log("hello form create",command);
 		tmp_PjtName = args[1];
-		console.log(tmp_PjtName);
+		// console.log(tmp_PjtName);
 	}
 
 	childProcess.stdout.on('data', data => {
 		this.emit('output', data.toString());
-		console.log(data.toString().trim());
+		// console.log(data.toString().trim());
 		var output = data.toString().trim();
 
 		// console.log(process.cwd());
 		// var current_directory = "/home/ubuntu/try.sciunit_10262017/test_cwd";
 		if (commandName == "sciunit" && args[2] == "copy"){
-			console.log('Starting directory: ' + process.cwd());
+			// console.log('Starting directory: ' + process.cwd());
 			try {
 				//Saving the path of current opened sciunit
 				var new_dir_nm = tmp_PjtName.slice(5,10);
@@ -52,7 +52,7 @@ CommandRunner.prototype.run = function run({ command, _spawn = spawn }) {
 
 				//Changing current working directory
 				process.chdir(new_dir_nm+'/');
-				console.log('New directory: ' + process.cwd());
+				// console.log('New directory: ' + process.cwd());
 
 				shell.cp( '/home/ubuntu/try.sciunit_10262017/hello.sh', '/home/ubuntu/try.sciunit_10262017/'+new_dir_nm);
 
