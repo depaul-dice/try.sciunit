731bff3f3bfad2845ee15b91c24f878912ac5af0
chilliyo <coxonerica@gmail.com>
2017-12-05 00:20:39 -0600

    change screen height and scroll box height

--- src/frontend/components/App/index.jsx
+++ src/frontend/components/App/index.jsx
@@ -94,12 +94,6 @@ function App(props) {
 	console.log(typeof(element));
 	if (element[0] != undefined){
 		console.log("scrollHeight",element[0].scrollHeight);
-
-		console.log("Before",element[0].scrollTop);
-
-		element[0].scrollTop = element[0].scrollHeight - 100;
-		console.log("After",element[0].scrollTop);
-
 		console.log("Here should scroll down");
 	}
 	// console.log(typeof(element));
