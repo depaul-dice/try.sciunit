1c5b199688ee073c4e67f89b1b14323c7775efbe
chilliyo <coxonerica@gmail.com>
2017-12-12 22:10:42 -0600

    change index.jsx

--- src/frontend/components/App/index.jsx
+++ src/frontend/components/App/index.jsx
@@ -1,9 +1,13 @@
 'use strict';
 const React = require('react');
 var ReactRouter = require('react-router');
+// const scrollIntoViewIfNeeded = require('scroll-into-view-if-needed');
 var autofocus = true;
 var parse = require('shell-quote').parse;
 var ReactCSSTransitionGroup = require('react-addons-css-transition-group'); // ES5
+var ReactDOM = require('react-dom');
+var element = document.getElementsByClassName("scroll-box");
+var emptydiv = document.getElementsByClassName("empty-div");
 
 var cmd_id = 0;
 var switch_detail_byID;
@@ -13,22 +17,18 @@ var copy_token_flag = false;
 var clicked_button_cmd = "";
 var session_number="1";
 var btn_blink_style = null;
-btn_blink_style ={
-margin: "-8px", paddingLeft:"70px",fontFamily:"Courier New", fontWeight: "bold"
-// 	animation-duration: 1200ms;
-// animation-name: blink;
-// animation-iteration-count: infinite;
-// animation-direction: alternate;
-// -webkit-animation:blink 1200ms;
-}
+var _scrollTop = "";
+btn_blink_style ={margin: "-11px", paddingLeft:"70px",fontFamily:"Lato",color:"Black"};
 
 var switch_detail;
+
 
 function App(props) {
 	let textInput = null;
 	let button_ref = null;
 	let menu_ref = null;
 	let detail_ref = null;
+	let endOftheOutput = null;
 	// console.log(props);
 	var output_fromSciunit=['sciunit create Project1'];
 	const { output, runCommand,cmd_detail,clickedButton} = props;
@@ -39,7 +39,7 @@ function App(props) {
 	function if_button_exist() {
 		if (cmd_detail[cmd_id].cmd_button != "")
 		{
-			return 	<button type="button" value={cmd_detail[cmd_id].cmd_button+copy_token} className="button-one"  ref={(input) => { button_ref = input; }} onMouseDown={handleMouseDown}>{cmd_detail[cmd_id].cmd_button}{copy_token}</button>
+			return 	<button type="button" style={{fontFamily:"Consolas"}} value={cmd_detail[cmd_id].cmd_button+copy_token} className="button-one" ref={(input) => { button_ref = input; }} onMouseDown={handleMouseDown} onMouseUp={handlefoucs}>{cmd_detail[cmd_id].cmd_button}{copy_token}</button>
 		}
 	};
 
@@ -53,11 +53,10 @@ function App(props) {
 			copy_token_flag = true;
 			session_number="2";
 			btn_blink_style ={
-				margin: "-8px",
-				color:"red",
+				margin: "-11px",
+				color:"#2d4d80",
 				paddingLeft:"70px",
-				fontFamily:"Courier New",
-				fontWeight: "bold",
+				fontFamily:"Lato",
 				animationDuration: "1200ms",
 				animationName: "blink",
 				animationDirection: "alternate",
@@ -65,34 +64,50 @@ function App(props) {
 
 			}
 			copy_token = "";
-			return <li key={o.id} className="output__item"><pre>> {o.value}</pre></li>
+			return <li key={o.id} className="output__item"><pre style={{fontFamily:"Ubuntu Mono"}}>> {o.value}</pre></li>
 		}
 
 		if (o.value.slice(8,15) == 'exec -i'){
 			copy_token = "";
-			return <li key={o.id} className="output__item"><pre>{o.value}</pre></li>
+			return <li key={o.id} className="output__item"><pre style={{fontFamily:"Ubuntu Mono"}}>{o.value}</pre></li>
 		}
 
 		if ((o.value.slice(0,7) === 'sciunit' || o.value.slice(0,2) === 'ls' || o.value.slice(0,3) === 'man' ) && user_input_command.length < 5){
 			copy_token = "";
-			return <li key={o.id} className="output__item"><pre>> {o.value}</pre></li>
+			return <li key={o.id} className="output__item"><pre style={{fontFamily:"Ubuntu Mono"}}>> {o.value}</pre></li>
 		}
 
 		else if (o.value.length == 8){
 			copy_token = o.value;
 			// console.log(copy_token);
-			return <li key={o.id} className="output__item"><pre> {o.value}</pre></li>
+			return <li key={o.id} className="output__item"><pre style={{fontFamily:"Ubuntu Mono"}}> {o.value}</pre></li>
 		}
 		else if (o.value.slice(8,12) == 'open')
 		{
 			// console.log("hello from open");
-			return <li key={o.id} className="output__item"><pre> {o.value}</pre></li>
+			return <li key={o.id} className="output__item"><pre style={{fontFamily:"Ubuntu Mono"}}> {o.value}</pre></li>
 		}
 		else {
 			// console.log(o.value);
-			return <li key={o.id} className="output__item"><pre> {o.value}</pre></li>
+			return <li key={o.id} className="output__item"><pre style={{fontFamily:"Ubuntu Mono"}}> {o.value}</pre></li>
 		}
 	});
+	// scrollIntoViewIfNeeded(emptydiv);
+	// console.log(element);
+	if (element[0] != undefined){
+		// console.log("scrollHeight",element[0].scrollHeight);
+		// console.log("Before",element[0].scrollTop);
+		var scrollHeight = element[0].scrollHeight;
+		scrollHeight = scrollHeight + 13;
+		// console.log("saved scrollHeight",scrollHeight);
+		var height = element[0].clientHeight - 13;
+		// console.log(height);
+		element[0].scrollTop = scrollHeight - height;
+		// console.log("After",element[0].scrollTop);
+		// console.log("Here should scroll down");
+	}
+	// console.log(typeof(element));
+
 	var user_input_command;
 	var command_key = ['Create','Exec','Show','Repeat','List','Copy','Open'];
 	var length_of_cmd_detail = cmd_detail.length;
@@ -120,7 +135,9 @@ function App(props) {
 		}
 
 	};
+
 	const nextStep = (e) => {
+
 		// console.log(user_input_command);
 		// console.log(cmd_id);
 		// console.log(,button_ref.value.trim(" "),user_input_command.trim(" "),("sicunit open "+copy_token).trim(" "),user_input_command === button_ref.value, user_input_command === "sicunit open "+copy_token);
@@ -146,7 +163,7 @@ function App(props) {
 				// console.log('cmd_id', cmd_id);
 			}
 		}
-
+		// console.log(_scrollTop);
 
 	};
 
@@ -168,11 +185,11 @@ function App(props) {
 		// switch_detail = e.target.getAttribute('value');
 		// console.log(switch_detail);
 		// console.log(switch_detail === 'Create',switch_detail === 'Exec')
-        //
+		//
 		// if (switch_detail === 'Create'){
 		// 	cmd_id = 0;
 		// 	console.log("YES");
-        //
+		//
 		// }
 		// else if (switch_detail === 'Exec'){
 		// 	cmd_id = 1;
@@ -188,7 +205,7 @@ function App(props) {
 		// 		switch_detail_byID = i;
 		// 	}
 		// }
-        //
+		//
 		// console.log(switch_detail_byID);
 		// cmd_id = switch_detail_byID;
 		// // console.log(cmd_detail[cmd_id]);
@@ -218,37 +235,40 @@ function App(props) {
 
 						<ol className="progress-bar">
 							<li>
-								<img src="https://sciunit.run/static/assets/images/new_GeoTrust.png" height="30" width="40" alt=""/>
+								{/*<img src="/frontend/new_GeoTrust_copy.png"  alt=""/>*/}
+								<a href="https://try.sciunit.run/"><img src="https://image.ibb.co/bMCpCw/new_Geo_Trust.png" height="35" width="35" border="0" /></a>
 							</li>
 							<li>
 								{/*<a value="Create" onClick={switchDetail} >*/}
-									{/*Create*/}
+								{/*Create*/}
 
 								{/*</a>*/}
-								<a value="0" onMouseDown={menu_selection}>Create</a>
+								<a value="0" onMouseDown={menu_selection} style={{marginTop: "3px"}}>
+									Create
+								</a>
 							</li>
 							<li>
-								<a value="1" onMouseDown={menu_selection}>Exec</a>
-							</li>
-
-							<li>
-								<a value="Show">Show</a>
+								<a value="1" onMouseDown={menu_selection} style={{marginTop: "3px"}}>Exec</a>
 							</li>
 
 							<li>
-								<a value="Repeat">Repeat</a>
+								<a value="Show" style={{marginTop: "3px"}}>Show</a>
 							</li>
 
 							<li>
-								<a value="List">List</a>
+								<a value="Repeat" style={{marginTop: "3px"}}>Repeat</a>
 							</li>
 
 							<li>
-								<a value="Copy">Copy</a>
+								<a value="List" style={{marginTop: "3px"}}>List</a>
 							</li>
 
 							<li>
-								<a value="Open">Open</a>
+								<a value="Copy" style={{marginTop: "3px"}}>Copy</a>
+							</li>
+
+							<li>
+								<a value="Open" style={{marginTop: "3px"}}>Open</a>
 							</li>
 
 							{/*<li>*/}
@@ -267,11 +287,11 @@ function App(props) {
 					<h1>{cmd_detail[cmd_id].title}</h1>
 					<hr/>
 					<div>
-						<span style={{fontFamily:"Courier New"}}>{cmd_detail[cmd_id].sciunit_1}</span>{cmd_detail[cmd_id].description_1}<span style={{fontFamily:"Courier New"}}>{cmd_detail[cmd_id].sciunit_1_1}</span> <br/>
+						<span style={{fontFamily:"Consolas", fontWeight:"bold"}}>{cmd_detail[cmd_id].sciunit_1}</span>{cmd_detail[cmd_id].description_1}<span style={{fontFamily:"Consolas", fontWeight:"bold"}}>{cmd_detail[cmd_id].sciunit_1_1}</span> <br/>
 						{cmd_detail[cmd_id].description} <br/>
-						{cmd_detail[cmd_id].description_2_1}<span style={{fontFamily:"Courier New"}}>{cmd_detail[cmd_id].sciunit_2}</span>{cmd_detail[cmd_id].description_2}<span style={{fontFamily:"Courier New"}}>{cmd_detail[cmd_id].sciunit_2_1}</span> <br/>
+						{cmd_detail[cmd_id].description_2_1}<span style={{fontFamily:"Consolas", fontWeight:"bold"}}>{cmd_detail[cmd_id].sciunit_2}</span>{cmd_detail[cmd_id].description_2}<span style={{fontFamily:"Consolas", fontWeight:"bold"}}>{cmd_detail[cmd_id].sciunit_2_1}</span> <br/>
 						{cmd_detail[cmd_id].description} <br/>
-						{cmd_detail[cmd_id].description_3}<span style={{fontFamily:"Courier New"}}>{cmd_detail[cmd_id].sciunit_3}</span>{cmd_detail[cmd_id].description_3_1}<span style={{fontFamily:"Courier New"}}>{cmd_detail[cmd_id].sciunit_4}</span>{cmd_detail[cmd_id].description_3_2} <br/>
+						{cmd_detail[cmd_id].description_3}<span style={{fontFamily:"Consolas", fontWeight:"bold"}}>{cmd_detail[cmd_id].sciunit_3}</span>{cmd_detail[cmd_id].description_3_1}<span style={{fontFamily:"Consolas", fontWeight:"bold"}}>{cmd_detail[cmd_id].sciunit_4}</span>{cmd_detail[cmd_id].description_3_2} <br/>
 					</div>
 					{if_button_exist()}
 				</div>
@@ -284,17 +304,18 @@ function App(props) {
 					</div>
 				</div>
 				<div className="fakeScreen" onClick={handlefoucs}>
-					<div className="scroll-box">
+					<div className="scroll-box" style={{scrollTop:_scrollTop}} >
 						Press enter to submit commands
 						<ul className="terminal--output">{outputChildren}</ul>
 						><input className="terminal__input" type="text" ref={(input) => { textInput = input; }} onKeyUp={runCommand} onKeyPress={nextStep} onKeyDown={getinput}/>
+						<div className="empty-div"></div>
 					</div>
 				</div>
 			</div>
 			<footer class="footer">
 				<a href="https://sciunit.run/" style={{fontSize: "22px", paddingLeft:"20px"}}>Sciunit Home</a>		<a href="https://sciunit.run/docs/" style={{fontSize: "22px", paddingLeft:"20px"}}>Documentaion</a><br/>
 				The Sciunit is supported by the National Science Foundation under grants ICER-1639759,<br/>
-					ICER-1661918, ICER-1440327, and ICER-1343816, and by Bloomberg Philanthropies (a UChicago subcontract).
+				ICER-1661918, ICER-1440327, and ICER-1343816, and by Bloomberg Philanthropies (a UChicago subcontract).
 			</footer>
 		</div>
 	);
