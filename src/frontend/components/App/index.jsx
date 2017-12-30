3c866017e55c7d57a294d506e266b58015bb73e3
chilliyo <coxonerica@gmail.com>
2017-12-29 23:30:45 -0600

    arrage programatically html of footer

--- src/frontend/components/App/index.jsx
+++ src/frontend/components/App/index.jsx
@@ -313,7 +313,10 @@ function App(props) {
 				</div>
 			</div>
 			<footer class="footer">
-				<a href="https://sciunit.run/" style={{fontWeight:"bold", fontSize: "1.15rem", paddingLeft:".5rem .5rem!important"}}>Sciunit Home</a>		<a href="https://sciunit.run/docs/" style={{fontSize: "22px", paddingLeft:"20px"}}>Documentaion</a><br/>
+				<div>
+					<a href="https://sciunit.run/" style={{fontWeight:"bold", fontSize: "1.15rem", paddingLeft:".5rem .5rem!important"}}>Sciunit Home</a>
+					<a href="https://sciunit.run/docs/" style={{fontSize: "22px", paddingLeft:"20px"}}>Documentaion</a>
+				</div>
 				<span style={{color:"#cecece", fontSize:"0.8rem"}}>Sciunit is supported by the National Science Foundation under grants ICER-1639759, ICER-1661918, ICER-1440327, and ICER-1343816, and by Bloomberg Philanthropies (a UChicago subcontract).</span>
 			</footer>
 		</div>
