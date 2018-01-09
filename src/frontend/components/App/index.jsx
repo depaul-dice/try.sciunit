ad99db5507422d60854c1e4e099d440cf874d943
chilliyo <coxonerica@gmail.com>
2018-01-08 20:36:33 -0600

    install react-arrow-key

--- src/frontend/components/App/index.jsx
+++ src/frontend/components/App/index.jsx
@@ -1,5 +1,6 @@
 'use strict';
 const React = require('react');
+const ArrowKeysReact = require('arrow-keys-react');
 var ReactRouter = require('react-router');
 // const scrollIntoViewIfNeeded = require('scroll-into-view-if-needed');
 var autofocus = true;
@@ -212,6 +213,10 @@ function App(props) {
 		// console.log("the globale in switch after ", cmd_id);
 
 	};
+	function () {
+
+	}
+
 	// console.log("the globale", cmd_id);
 	// const switchDetail = (e) => {
 	// 	// console.log("the globale in switch", cmd_id);
@@ -314,8 +319,8 @@ function App(props) {
 			</div>
 			<footer class="footer">
 				<div>
-					<a href="https://sciunit.run/" style={{fontWeight:"bold", fontSize: "1.15rem", paddingLeft:".5rem .5rem!important"}}>Sciunit Home</a>
-					<a href="https://sciunit.run/docs/" style={{fontSize: "22px", paddingLeft:"20px"}}>Documentaion</a>
+					<a href="https://sciunit.run/" style={{ fontSize: "1.15rem", paddingLeft:".5rem .5rem!important"}}>Sciunit Home</a>
+					<a href="https://sciunit.run/docs/" style={{fontSize: "1.15rem", paddingLeft:".5rem .5rem!important"}}>Documentaion</a>
 				</div>
 				<span style={{color:"#cecece", fontSize:"0.8rem"}}>Sciunit is supported by the National Science Foundation under grants ICER-1639759, ICER-1661918, ICER-1440327, and ICER-1343816, and by Bloomberg Philanthropies (a UChicago subcontract).</span>
 			</footer>
